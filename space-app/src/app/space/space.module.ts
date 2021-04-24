bc0c2a87733daad9c31549203018b5db689bc495
Marek <marekfirlit.biz@gmail.com>
2021-04-24 17:37:13 +0100

    zmiana tła oraz dodanie statuku i styli css

--- space-app/src/app/space/space.module.ts
+++ space-app/src/app/space/space.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HangarComponent } from './hangar/hangar.component';
+import { SpaceShipComponent } from './space-ship/space-ship.component';
 
 
 
 @NgModule({
   declarations: [
-    HangarComponent
+    HangarComponent,
+    SpaceShipComponent
+    
   ],
-  exports:[
+  exports: [
     HangarComponent
   ],
   imports: [
     CommonModule
+    
   ]
 })
 export class SpaceModule { }
